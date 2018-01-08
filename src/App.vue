3f5d9788df954885ee62f2355914767cf52c831d
adexphem <adexphem4real@gmail.com>
2018-01-08 10:29:38 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <app-quote quote="A wonderful quote!"></app-quote>
+  </div>
+</template>
+
+<script>
+  import Quote from './components/Quote.vue';
+
+  export default {
+    components: {
+      appQuote: Quote
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>{{ quote }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['quote']
+  }
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px Black;
+    padding: 20px;
+    margin: 30px auto;
+    text-align: center;
+  }
+</style>
